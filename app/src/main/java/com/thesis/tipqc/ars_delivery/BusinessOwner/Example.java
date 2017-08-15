f1c72c226e284b651f40a7b204e144caee7c4baa
josrp172 <josrp172@gmail.com>
2017-08-15 13:56:55 +0800

    Initial Commit

--- app/src/main/java/com/thesis/tipqc/ars_delivery/BusinessOwner/Example.java
+++ app/src/main/java/com/thesis/tipqc/ars_delivery/BusinessOwner/Example.java
@@ -13,29 +13,6 @@ import com.google.android.gms.maps.model.LatLng;
 public class Example {
 
     public static void main(String[] args){
-        GoogleDirection.withServerKey("");
 
-        String key = "AIzaSyA_Vj68cTwVJVDQiht3Aw4-sBh8QRvMvTw";
-
-        GoogleDirection.withServerKey(key)
-                .from(new LatLng(37.7681994, -122.444538))
-                .to(new LatLng(37.7749003,-122.4034934))
-                .avoid(AvoidType.FERRIES)
-                .avoid(AvoidType.HIGHWAYS)
-                .execute(new DirectionCallback() {
-                    @Override
-                    public void onDirectionSuccess(Direction direction, String rawBody) {
-                        if(direction.isOK()) {
-                            System.out.println(direction.getRouteList().toString());
-                        } else {
-                            // Do something
-                        }
-                    }
-
-                    @Override
-                    public void onDirectionFailure(Throwable t) {
-                        // Do something
-                    }
-                });
     }
 }
